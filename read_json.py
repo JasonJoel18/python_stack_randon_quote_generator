d72d5717c340d1a0407cbe62d6aa958244ff298d
JasonJoel18 <159021390+JasonJoel18@users.noreply.github.com>
2024-02-20 00:32:50 +0100

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -6,13 +6,13 @@ f = open('quotes.json')
 quotes_list = json.load(f)
 
 def getQuote(quotes_list = quotes_list):
-    """_summary_
+    """This functions takes dictionary of quotes as the input and selects and object/quote from the dictionary.
 
     Args:
-        quotes_list (_type_, optional): _description_. Defaults to quotes_list.
+        quotes_list (dictionary, optional): This is dictionary with quotes that we create. Defaults to quotes_list.
 
     Returns:
-        _type_: _description_
+        dictionary: returned in the form of dictionary
     """
     # random_int = random.randint(0,len(quotes_list)-1)
     # selected_obj = quotes_list[random_int]
